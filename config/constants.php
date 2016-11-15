f07a92f39de2bc64418700495b2bc2023befe1ac
Vincent Lore <vincent_lore@hotmail.com>
2016-11-14 23:58:40 -0800

    Add external API URL constants

--- config/constants.php
+++ config/constants.php
@@ -1,5 +1,23 @@
 <?php 
 
+if (!defined('NHL_GOALIES')) {
+	define('NHL_GOALIES', 'http://www.nhl.com/stats/rest/grouped/goalies/season/');
+}
+
+
+if (!defined('NHL_SKATERS')) {
+    define('NHL_SKATERS', 'http://www.nhl.com/stats/rest/grouped/skaters/season/');
+}
+
+
 return [
-    'TEAMS_API_URL' => 'https://statsapi.web.nhl.com/api/v1/teams'
+    'SEASON_ID' => '20162017',
+    'REGULAR_SEASON' => '2',
+    'PLAYOFFS' => '3',
+    'TEAMS_API_URL' => 'https://statsapi.web.nhl.com/api/v1/teams',
+    'SKATERS_BIO_API' => NHL_SKATERS . 'bios',
+    'SKATERS_STATS_API' => NHL_SKATERS . 'skatersummary',
+    'SKATERS_STATS_API2' => NHL_SKATERS . 'realtime',
+    'GOALIES_BIO_API' => NHL_GOALIES . 'bios',
+    'GOALIES_STATS_API' => NHL_GOALIES . 'goaliesummary'
 ];
