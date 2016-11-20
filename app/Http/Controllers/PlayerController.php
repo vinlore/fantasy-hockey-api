0d41199c4f4575e27b009f661b7cd03036a5646a
Vincent Lore <vincent_lore@hotmail.com>
2016-11-20 04:19:03 -0800

    Add GET methods for players

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use App\Models\Player;
 
 class PlayerController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        try {
+            return Player::with('statsYear')->get();
+        } catch (Exception $e) {
+            return response()->error();
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            $player = Player::find($id);
+
+            if (!$player) {
+                return response()->error('404', 'Player not found.');
+            }
+
+            return $player;
+        } catch (Exception $e) {
+            return response()->error();
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,13 @@ use Illuminate\Http\Request;
 
 Route::group(['middleware' => ['api']], function () {
 
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST,GET,OPTIONS');
+    header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
     Route::post('/auth/register', 'AuthenticateController@register');
     Route::post('/auth/login', 'AuthenticateController@login');
 
+    Route::resource('players', 'PlayerController', ['only' => ['index', 'show']]);
+    
 });
